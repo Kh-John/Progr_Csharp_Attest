18eaeaed54338fb6e4ccf81aa2085777d18a5c0a
Ivan <khjohn135@gmail.com>
2024-02-23 22:59:54 +0600

    add Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index < 0)
+            return;
+
+        Console.Write(array[index]);
+        
+        if (index > 0)
+        {
+            Console.Write(" ");
+        }
+
+        PrintArrayReverse(array, index - 1);
+    }
+}
+
